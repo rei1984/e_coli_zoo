ba4bbe1adda83fda9efbbdf9d03b914f408df263
rei1984 <Morgan.A.S.Jones@warwick.ac.uk>
2021-01-18 19:48:18 +0000

    mutations cause change in colour

--- agent.js
+++ agent.js
@@ -1,14 +1,4 @@
 
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    // isOccupied() {
-    //     return space[x][y];
-    // }
-}
-
 class Gene {
     constructor(val) {
         this.val = val;
@@ -28,9 +18,12 @@ class Gene {
 }
 
 class Genome {
-    constructor() {
-        this.genes = [];
+    constructor(genes) {
+        this.genes = genes;
     }
+    // constructor(genes) {
+    //     this.genes = genes;
+    // }
     //get the list of genes
     getGenes() {
         return this.genes;
@@ -55,33 +48,48 @@ class Genome {
 }
 
 class Agent {
-    constructor(genome, pos, id) {
+    constructor(genome, x, y, id) {
         this.genome = genome;
-        this.position = pos;
+        // this.position = createVector(x, y);
         this.id = id;
-        this.instability = Math.random();
-        this.velocity = [0, 0];
-        this.maxSpeed = 1;
-        this.maxForce = 0.03;
+        this.velocity = new p5.Vector(0, 0);
+        this.position = createVector(x, y);
+        this.r = 3.0;
+        this.maxspeed = 0.5;    // Maximum speed
+        this.maxforce = 0.02; // Maximum steering force
+        this.splitrecovery = 100;
     }
     getGenome() {
         return this.genome;
     }
     split() {
         //get a location for split
-        var loc = this.getSplitSpace();
+        // var loc = this.getSplitSpace();
         // console.log("Splitting...");
         // console.log(loc);
 
-        if (loc == null) return null;
+        // if (loc == null) return null;
 
         // console.log("Splitting going ahead");
         //create new genome copy
+        
         var newGenome = mutate(this.genome);
         
         var newID = 2;
 
-        var offspring = new Agent(newGenome, loc, newID);
+        let r = Math.random();
+
+        if (r < 0.25) {
+            var offspring = new Agent(newGenome, this.position.x + 1, this.position.y + 1, newID);
+        } else if (r < 0.5) {
+            var offspring = new Agent(newGenome, this.position.x + 1, this.position.y - 1, newID);
+        } else if (r < 0.75) {
+            var offspring = new Agent(newGenome, this.position.x - 1, this.position.y + 1, newID);
+        } else {
+            var offspring = new Agent(newGenome, this.position.x - 1, this.position.y - 1, newID);
+        }
+
+        
 
         console.log(this.id + " SPLIT into " + newID + " with GENOME:[ " + newGenome.toString() + " ]...");
 
@@ -92,22 +100,69 @@ class Agent {
 
         return offspring;
     }
-    getCohesion(colony) {
-        return [0, 0];
-    }
-    getSeparation(colony) {
-        let idealSep = 10;
-        let o = [0, 0];
-        let count = 0;
-        for (var c in colony) {
-            let d = vectorDiff(this.position, c.position);
-            if (d > 0 && d < idealSep) {
-                let diff = VectorDiff(this.position, c.position);
-                let norm = VectorNorm(diff);
-                o = vectorSum(o, norm);
-            }
-        }
 
+    seek(target) {
+        let desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
+        // Normalize desired and scale to maximum speed
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        // Steering = Desired minus Velocity
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        return steer;
+      }
+
+    getCohesion(colony) {
+        let neighbordist = 100;
+        let sum = createVector(0, 0); // Start with empty vector to accumulate all locations
+        let count = 0;
+        for (let i = 0; i < colony.length; i++) {
+          let d = p5.Vector.dist(this.position, colony[i].position);
+          if ((d > 0) && (d < neighbordist)) {
+            sum.add(colony[i].position); // Add location
+            count++;
+          }
+        }
+        if (count > 0) {
+          sum.div(count);
+          return this.seek(sum); // Steer towards the location
+        } else {
+          return createVector(0, 0);
+        }
+    }
+    //TODO: Merge this and cohesion to save computation time
+    getSeparation(colony) {
+        let desiredseparation = 20.0;
+        let steer = createVector(0, 0);
+        let count = 0;
+        // For every boid in the system, check if it's too close
+        for (let i = 0; i < colony.length; i++) {
+          let d = p5.Vector.dist(this.position, colony[i].position);
+          // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+          if ((d > 0) && (d < desiredseparation)) {
+            // Calculate vector pointing away from neighbor
+            let diff = p5.Vector.sub(this.position, colony[i].position);
+            diff.normalize();
+            diff.div(d); // Weight by distance
+            steer.add(diff);
+            count++; // Keep track of how many
+          }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+          steer.div(count);
+        }
+      
+        // As long as the vector is greater than 0
+        if (steer.mag() > 0) {
+          // Implement Reynolds: Steering = Desired - Velocity
+          steer.normalize();
+          steer.mult(this.maxspeed);
+          steer.sub(this.velocity);
+          steer.limit(this.maxforce);
+        }
+        return steer;
+      }
         // var o = [0, 0];
         // for (var c of colony) {
         //   let distVector = vectorDiff(this.position, c.position);
@@ -117,27 +172,78 @@ class Agent {
         //   }
         // }
         // return o;
-      }
     getActingForces(colony) {
         let cohesion = this.getCohesion(colony);
         // let seperation = vectorScalar(this.getSeparation(colony), 1 * this.instability);
         let seperation = this.getSeparation(colony);
-        let ActingForces = [];
-        ActingForces.push(cohesion[0] + seperation[0]);
-        ActingForces.push(cohesion[1] + seperation[1]);
+
+        sep.mult(1.4);
+        // ali.mult(1.0);
+        coh.mult(1.1);
+        
+        if (sep.x == 0 && sep.y == 0 && coh.x == 0 && coh.y == 0) {
+            this.acceleration.set(0, 0);
+            this.velocity.set(0, 0);
+        }
+
         return ActingForces;
     }
-    updateVelocity(colony) {
-        let actingForces = this.getActingForces(colony);
-        // console.log("Acting force = " + actingForces);
-        this.velocity[0] = actingForces[0] + this.velocity[0];
-        this.velocity[1] = actingForces[1] + this.velocity[1];
-        return this.velocity;
-    }
+
     updatePosition() {
-        this.position = vectorSum(this.position, this.velocity);
+        this.position.add(this.velocity);
         return this.position;
     }
+    updateVelocity(colony) {
+        // let coh = this.getCohesion(colony);
+        let coh = createVector(0, 0);
+        // let seperation = vectorScalar(this.getSeparation(colony), 1 * this.instability);
+        let sep = this.getSeparation(colony);
+
+        var acceleration = createVector(0, 0);
+
+        sep.mult(1.8);//1.4
+        // ali.mult(1.0);
+        // coh.mult(1);
+
+        //if we have no need to seek or move away from another bacterium stop and don't move
+        if (sep.x == 0 && sep.y == 0 && coh.x == 0 && coh.y == 0) {
+            this.velocity.set(0, 0);
+        } else {
+            acceleration.add(sep);
+            // acceleration.add(coh);
+            this.velocity.add(acceleration);
+            this.velocity.limit(this.maxspeed);
+            this.updatePosition();
+        }
+
+        
+
+
+    }
+
+
+
+    run(colony) {
+        // this.flock(boids);
+        let offspring = null;
+        this.updateVelocity(colony);
+        this.borders();
+        this.show();
+        let m = Math.random();
+        if (m < 0.01 && colony.length < 20 && this.splitrecovery > 100) {
+          offspring = this.split();
+          this.splitrecovery = 0;
+        }
+        if (m < 0.02 && m > 0.017) {
+          var i = colony.indexOf(this);
+          colony.splice(i, 1);
+          return null;
+        }
+        this.splitrecovery++;
+        return offspring;
+        // this.split();
+    }
+
     toString() {
         let no = 0;
         for (var i = 0; i < this.genome.getGenes().length; i++) {
@@ -158,58 +264,54 @@ class Agent {
           this.position.y = 600;
         }
       }
+
+    borders() {
+    if (this.position.x < -this.r) this.position.x = width + this.r;
+    if (this.position.y < -this.r) this.position.y = height + this.r;
+    if (this.position.x > width + this.r) this.position.x = -this.r;
+    if (this.position.y > height + this.r) this.position.y = -this.r;
+    }
     getSplitSpace() {
 
         return [this.position[0] + 5, this.position[1] + 5];
     
     }
-        // let possibleLocs = [];
-    
-        
-    
-        // for (var i = 0; i < 3; i++) {
-    
-        //     if (space[pos.x + 5*i][pos.y] == " ") possibleLocs.push(new Point(pos.x + 5*i, pos.y));
-        //     if (space[pos.x - 5*i][pos.y] == " ") possibleLocs.push(new Point(pos.x - 5*i, pos.y));
-        //     if (space[pos.x][pos.y + 5*i] == " ") possibleLocs.push(new Point(pos.x, pos.y + 5*i));
-        //     if (space[pos.x][pos.y - 5*i] == " ") possibleLocs.push(new Point(pos.x, pos.y - 5*i));
-        
-        // }
-        // if (possibleLocs.length == 0) return null;
-    
-        // return (possibleLocs[Math.floor(Math.random()*possibleLocs.length)]);
-    
-        // return [pos.x][pos.y + 1];
-    
-    // }
+
     show() {
-        strokeWeight(10);
-        color('purple')
-        // let r = this.genome.getGenes()[0].toInteger();
-        // let g = this.genome.getGenes()[1].toInteger();
-        // let b = this.genome.getGenes()[2].toInteger();
-        // color(r, g, b);
-        circle(this.position.x, this.position.y, 10);
+        // strokeWeight(10);
+        // fill(127, 127);
+        stroke(200);
+        let r = this.genome.getGenes()[0].toInteger();
+        let g = this.genome.getGenes()[1].toInteger();
+        let b = this.genome.getGenes()[2].toInteger();
+        fill(r, g, b);
+        ellipse(this.position.x, this.position.y, 16, 16);
     }
 }
 
 //TODO: mutation function
 function mutate(genome) {
-    if (Math.round(Math.random() * (100)) > MUTP) return genome;
+    // if (Math.round(Math.random() * (100)) > 5) return genome;
 
-    mutatedFlag = 1;
+    // mutatedFlag = 1;
+
+    let copy = genome.getGenes().slice();
     
-    //choose a base location for mutation
-    let mutagen = (Math.round(Math.random() * (genome.getGenes().length)) > genome.getGenes().length);
+    if (Math.random() < 0.05) {
+        console.log("MUTATION!");
+        //choose a base location for mutation
+        let mutagen = Math.floor(Math.random() * copy.length);
 
-    //mutate that space with a random base
-    let base = (["A", "C", "T", "G"][Math.floor(Math.random()*4)])
+        //mutate that space with a random base
+        let base = (["A", "C", "T", "G"][Math.floor(Math.random()*4)])
 
-    //set the gene
-    genome.setGene(mutagen, base);
+        //set the gene
+        copy[mutagen] = new Gene(base);
+
+    }
 
     //return the altered genome
-    return genome;
+    return new Genome(copy);
 }
 
 function printColony() {
@@ -221,4 +323,21 @@ function printColony() {
         console.log(arrText);
         arrText='';
     }
+}
+
+function getStarterAgent() {
+    var startGenome = new Genome([new Gene('A'), new Gene('C'), new Gene('C')]);
+    // startGenome.addGene(new Gene("A"));
+    // startGenome.addGene(new Gene("C"));
+    // startGenome.addGene(new Gene("C"));
+    // startGenome.addGene(new Gene("T"));
+    // startGenome.addGene(new Gene("G"));
+    // startGenome.addGene(new Gene("G"));
+    // startGenome.addGene(new Gene("A"));
+    // startGenome.addGene(new Gene("T"));
+    // startGenome.addGene(new Gene("T"));
+    // startGenome.addGene(new Gene("A"));
+    // startGenome.addGene(new Gene("A"));
+    // let startPos = [300, 300];
+    return new Agent(startGenome, 300, 300, 1);
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var colony = [];
+var Newboids = [];
 
 var sampleSpace = new Map();
 //create the study space
@@ -6,22 +7,8 @@ const MUTP = 10;
 const SIZE = 600;
 const MAX = 200;
 
-var startGenome = new Genome();
-startGenome.addGene(new Gene("A"));
-startGenome.addGene(new Gene("C"));
-startGenome.addGene(new Gene("C"));
-// startGenome.addGene(new Gene("T"));
-// startGenome.addGene(new Gene("G"));
-// startGenome.addGene(new Gene("G"));
-// startGenome.addGene(new Gene("A"));
-// startGenome.addGene(new Gene("T"));
-// startGenome.addGene(new Gene("T"));
-// startGenome.addGene(new Gene("A"));
-// startGenome.addGene(new Gene("A"));
-let startPos = [SIZE/2, SIZE/2];
-let starter = new Agent(startGenome, startPos, 1);
-colony.push(starter);
-let flag = 0;
+
+// let flag = 0;
 // sampleSpace[startPos] = starter;
 // space[SIZE/2][SIZE/2] = "@";
 
@@ -33,36 +20,44 @@ function setup() {
 //   for (let i = 0; i < MAX; i++) {
 //     flock.push(new Agent);
 //   }
+  colony.push(getStarterAgent());
 }
 
 function draw() {
   background(51);
   // if (colony.length < 10) {
-    
-    newAgents = [];
-    for (let agent of colony) {
-      // boid.(flock);
-      agent.updateVelocity(colony);
-      // console.log(agent.velocity);
-      agent.updatePosition();
-      agent.edges();
-      if (colony.length < 10) {
-      let offspring = agent.split();
-      // console.log(offspring);
-      newAgents.push(offspring);
-      // console.log(newAgents);
-      console.log(colony);
-      }
-      // agent.show();
-      strokeWeight(10);
-      // color('purple')
-      let r = agent.genome.getGenes()[0].toInteger();
-      let g = agent.genome.getGenes()[1].toInteger();
-      let b = agent.genome.getGenes()[2].toInteger();
-      color(r, g, b);
-      circle(agent.position[0], agent.position[1], 10);
-    }
-    colony = colony.concat(newAgents);
+  for (let i = 0; i < colony.length; i++) {
+    let o = colony[i].run(colony);
+    if (o != null) Newboids.push(o);
+  }
+  if (colony.length < 20) {
+      colony = colony.concat(Newboids);
+      Newboids = [];
+  }
+    // newAgents = [];
+    // for (let agent of colony) {
+    //   // boid.(flock);
+    //   agent.updateVelocity(colony);
+    //   // console.log(agent.velocity);
+    //   agent.updatePosition();
+    //   agent.edges();
+    //   if (colony.length < 10) {
+    //   let offspring = agent.split();
+    //   // console.log(offspring);
+    //   newAgents.push(offspring);
+    //   // console.log(newAgents);
+    //   console.log(colony);
+    //   }
+    //   // agent.show();
+    //   strokeWeight(10);
+    //   // color('purple')
+    //   let r = agent.genome.getGenes()[0].toInteger();
+    //   let g = agent.genome.getGenes()[1].toInteger();
+    //   let b = agent.genome.getGenes()[2].toInteger();
+    //   color(r, g, b);
+    //   circle(agent.position[0], agent.position[1], 10);
+    // }
+    // colony = colony.concat(newAgents);
   // }
   
   // color('purple');
